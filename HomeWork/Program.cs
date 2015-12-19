e09ec66d1bbdb494a78aa8837e3545315943ce20
Julia7777 <abramchenko.jul@yandex.ru>
2015-12-19 22:32:25 +0200

    ProjectSort

---
+++ HomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8, 5, 4, 6, 7, 1, 2, 3, 12, 9};
+            Console.WriteLine("Before sorting");
+            print(array);
+            QuickSort.Sort(array, 0, array.Length);
+            Console.WriteLine("After sorting");
+            print(array);
+            Console.ReadKey();
+        }
+        public static void print(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HomeWork/QuickSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork
+{
+    class QuickSort
+    {
+        public static void Sort(int[] array, int first, int count)
+        {
+            SortPrivate(array, first, first + count - 1);
+        }
+ 
+        static void SortPrivate (int [] array, int first, int last)
+        {
+            if (first < last)
+            {
+                int s = Separation (array, first, last);
+                SortPrivate (array, first, s - 1);
+                SortPrivate (array, s + 1, last);
+            }
+        }
+        static int Separation (int[] array, int first, int last)
+        { 
+            int pivot = array [last];
+            int temp;
+            int i = first;
+            for (int k = first; k < last; k++)
+            {
+                if (array[k] <= array[last])
+                {
+                    temp = array[k];
+                    array[k] = array[i];
+                    array[i] = temp;
+                    i++;
+                }
+            }
+            array[last] = array[i];
+            array [i] = pivot;
+
+            return i;
+        }
+      
+
+    }
+}
